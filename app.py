d21f143cb577521aedba978f78d0d151811b0f49
Achintya Singh <98767261+achiit@users.noreply.github.com>
2024-03-10 16:41:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def download_endpoint():
         return jsonify({"error": response}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(threaded=True, debug=True, host="0.0.0.0", port=3000)
